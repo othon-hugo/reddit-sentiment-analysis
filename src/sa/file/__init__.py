48a9b856ad22870d8c79ac49175d9cb04beb5b10
othon <162609004+othonhugo@users.noreply.github.com>
2026-02-23 16:06:57 +0000

    feat(file): introduce `XLSXColumnReader` for XLSX column extraction

--- src/sa/file/__init__.py
+++ src/sa/file/__init__.py
@@ -1,11 +1,12 @@
 from .csv import CSVPosts
-from .xlsx import XLSXPosts
+from .xlsx import XLSXPosts, XLSXColumnReader
 from .conveter import FileFormat, ConverterFactory
 
 
 __all__ = [
     "ConverterFactory",
     "CSVPosts",
+    "XLSXColumnReader",
     "XLSXPosts",
     "FileFormat",
 ]
